57917760eea1306f90bd5d92ad50796e99cf2a1d
Umayr Hassan <umayrh@gmail.com>
2020-02-04 15:00:16 -0800

    Daily problem 11

--- src/2020-02-04.py
+++ src/2020-02-04.py
@@ -7,17 +7,54 @@ For example, given the query string `de` and the set of strings `[dog, deer, dea
 Hint: Try preprocessing the dictionary into a more efficient data structure to speed up queries.
 """
 
-from typing import List
-Vector = List[str]
+from typing import Dict
+Map = Dict[str, Dict]
 
 
-def search_prefix(prefix: str, vec: Vector):
-    pass
+class UncompressedTrie:
+    def __init__(self):
+        self.trie: Map = {}
 
+    def add_all(self, words):
+        for word in words:
+            self.add(word)
 
-def build_index(vec: Vector):
-    pass
+    def add(self, word: str):
+        trie_level = self.trie
+        for idx in range(len(word)):
+            if not word[idx] in trie_level:
+                trie_level[word[idx]]: Map = {}
+            trie_level = trie_level[word[idx]]
+        trie_level["*"] = {word: {}}
+
+    def find(self, prefix: str):
+        if not self.trie:
+            return set()
+        trie_level = self.trie
+        for idx in range(len(prefix)):
+            if not prefix[idx] in trie_level:
+                return set()
+            trie_level = trie_level[prefix[idx]]
+        # find all words under this sub-trie
+        result = set()
+        self._find(trie_level, result)
+        return result
+
+    def _find(self, trie: Map, result: set):
+        if not trie:
+            return
+        if '*' in trie:
+            for key in trie['*'].keys():
+                result.add(key)
+        for key in trie:
+            self._find(trie[key], result)
 
 
 if __name__ == "__main__":
-    assert 3 == 3
+    trie = UncompressedTrie()
+    trie.add("dog")
+    assert "dog" in trie.find("dog")
+
+    trie.add("deer")
+    trie.add("deal")
+    assert set(["deer", "deal"]).issubset(trie.find("de"))
