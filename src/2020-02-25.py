f0b45b87061d165f12f3393a27981d5ad864ab7d
Umayr Hassan <umayrh@gmail.com>
2020-02-29 15:16:43 -0800

    Clean up #36

--- src/2020-02-25.py
+++ src/2020-02-25.py
@@ -1,50 +1,31 @@
 """
-The edit distance between two strings refers to the minimum number of character insertions,
-deletions, and substitutions required to change one string to the other. For example,
-the edit distance between “kitten” and “sitting” is three: substitute the “k” for “s”,
-substitute the “e” for “i”, and append a “g”.
+Suppose you are given a table of currency exchange rates, represented as a 2D array.
+Determine whether there is a possible arbitrage: that is, whether there is some sequence of
+trades you can make, starting with some amount A of any currency, so that you can end up
+with some amount greater than A of that currency.
 
-Given two strings, compute the edit distance between them.
+There are no transaction costs and you can trade fractional quantities.
 """
+import sys
 from typing import List
-Vector = List[int]
+Matrix = List[List[int]]
 
 
-def edit_distance(str1: str, str2: str) -> int:
-    if not str1:
-        return len(str2)
-    if not str2:
-        return len(str1)
-    str1_len = len(str1)
-    str2_len = len(str2)
-    shorter = str1 if str1_len < str2_len else str2
-    shorter_len = min(str1_len, str2_len)
-    longer = str2 if str1_len < str2_len else str1
-    longer_len = max(str1_len, str2_len)
-    longer_st = 0
-    max_matches = 0
-    # find the alignment with maximum matchings
-    for st in range(longer_len - shorter_len + 1):
-        matches = 0
-        for idx in range(shorter_len):
-            if shorter[idx] == longer[st + idx]:
-                matches += 1
-        if matches > max_matches:
-            max_matches = matches
-            longer_st = st
-    # count mis-alignments
-    distance = longer_st
-    for idx in range(longer_st, len(longer)):
-        if idx - longer_st < shorter_len:
-            if shorter[idx - longer_st] != longer[idx]:
-                distance += 1
-        else:
-            distance += 1
-    return distance
+def arbitrage(costs: Matrix) -> int:
+    """
+    st A B C A
+    :param costs:
+    :return:
+    """
+    rows = len(costs)
+    cols = len(costs[0])
+    distance: Matrix = [[sys.maxsize for _ in range(cols)] for _ in range(rows)]
+    for st in range(rows):
+        queue = [st]
+        while len(queue) > 0:
+            pass
+    pass
 
 
 if __name__ == "__main__":
-    assert edit_distance("kitten", "sitting") == 3
-    assert edit_distance("abcxyz", "xyz") == 3
-    assert edit_distance("abcxyz", "pqr") == 6
-    assert edit_distance("mississippi", 'missouri') == 6
+    assert arbitrage([[1, 2], [0.3, 1]]) > 0
