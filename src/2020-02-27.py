17c8eefb07a4cd42e2c13f2f53b7327b3d5a6583
Umayr Hassan <umayrh@gmail.com>
2020-02-29 02:56:40 -0800

    Bug fix for #34

--- src/2020-02-27.py
+++ src/2020-02-27.py
@@ -56,11 +56,23 @@ def _find_longest_palindrome_substr(s: str, s_len: int) -> (str, int, int):
             en += 1
         st += 1
         en -= 1
-        # idx = en # can I get away with jumping the gun here?
+        # can I get away with jumping the gun here?
+        # idx = en
         if en - st > longest_en - longest_st:
             longest = s[st:en+1]
             longest_st = st
             longest_en = en
+        # Amongst equals, select the palindrome bounded by the lexicographically smallest character
+        # FIXME maybe not quite right since we're only looking at the immediate bounding chars, which may be equal
+        elif en - st == longest_en - longest_st:
+            st_p = st - 1 if st > 0 else 0
+            en_p = en + 1 if en < s_len - 1 else s_len - 1
+            longest_st_p = longest_st - 1 if longest_st > 0 else 0
+            longest_en_p = longest_en + 1 if longest_en < s_len - 1 else s_len - 1
+            if min(s[st_p], s[en_p]) < min(s[longest_st_p], s[longest_en_p]):
+                longest = s[st:en+1]
+                longest_st = st
+                longest_en = en
         idx += 1
     print(f"{longest}, {longest_st}, {longest_en}")
     return longest, longest_st, longest_en
@@ -77,18 +89,23 @@ def make_palindrome(s: str) -> str:
     # Entire string is a palindrome
     if longest_st == 0 and longest_en == s_len - 1:
         return s
-    # Palindromic substring found
+    # Longest palindromic substring found
     if longest_st >= 0:
+        # The substring prefixes the string
         if longest_st == 0:
             prefix = "".join([s[idx] for idx in range(s_len - 1, longest_en, -1)])
             suffix = s
+        # The substring suffixes the string
         elif longest_en == s_len - 1:
             prefix = s
             suffix = "".join([s[idx] for idx in range(longest_st - 1, -1, -1)])
+        # The substring straddles the middle
         else:
             prefix = s[:longest_st]
             suffix = s[longest_en + 1:]
+            # Create lexicographically smallest prefix
             prefix = suffix + prefix if prefix > suffix else prefix + suffix
+            # Suffix is prefix reversed appended to the longest palindromic substring
             suffix = longest + "".join([prefix[idx] for idx in range(len(prefix) - 1, -1, -1)])
         return prefix + suffix
     # No palindromic substring. Create lexicographically smallest palindrome.
@@ -107,3 +124,5 @@ if __name__ == "__main__":
     assert make_palindrome("ooooooomooooooo1") == "1ooooooomooooooo1"
     assert make_palindrome("mommommom") == "mommommom"
     assert make_palindrome("1mommommom") == "1mommommom1"
+    assert make_palindrome("abcxxdefyyghizz") == "abcdefyyghizzxxzzihgyyfedcba"
+    assert make_palindrome("ghixxdefyyabczz") == "abczzghixxdefyyfedxxihgzzcba"
