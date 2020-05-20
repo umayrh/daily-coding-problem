5e51ac7368e5cd92519fc702ee575523b4f60a46
Umayr Hassan <umayrh@gmail.com>
2020-05-20 02:05:39 -0700

    Fix bug in #114

--- src/2020-05-17.py
+++ src/2020-05-17.py
@@ -4,29 +4,34 @@ relative order of the delimiters. For example, given "hello/world:here", return 
 
 Follow-up: Does your solution work for the following cases: "hello/world:here/", "hello//world:here"
 """
+import re
+
 
 def reverse(phrase, delimiters):
-    words = phrase.split(delimiters)
-    delim_idx = 0
-    delim_queue = []
-    for word in words:
-        delim_idx = delim_idx + len(word)
-        delim_queue.append(phrase[delim_idx])
+    words = re.split(delimiters, phrase)
+    if '' in words:
+        words.remove('')
 
-    result = ""
-    delim_idx = 0
-    if phrase[0] in delimiters:
-        result += delim_queue[delim_idx]
-        delim_idx += 1
-    for word_idx in range(len(words):
-        word = words[-word_idx]
+    delim_queue = []
+    delim_st = 0
+    phrase_len = len(phrase)
+    for idx in range(len(words)):
+        delim_en = phrase.find(words[idx], delim_st, phrase_len)
+        delim_queue.append(phrase[delim_st:delim_en])
+        delim_st = delim_en + len(words[idx])
+    delim_queue.append(phrase[delim_st:phrase_len])
+
+    delim_idx = 1
+    result = delim_queue[0]
+    for word_idx in range(len(words)):
+        result += words[-word_idx-1]
         if delim_idx < len(delim_queue):
             result += delim_queue[delim_idx]
             delim_idx += 1
-     return result
+    return result
 
 
 if __name__ == "__main__":
-    assert reverse("hello/world:here") == "here/world:hello"
-    assert reverse("hello/world:here/") == "here/world:hello/"
-    assert reverse("hello//world:here") == "here//world:hello"
+    assert reverse("hello/world:here", "/|:") == "here/world:hello"
+    assert reverse("hello/world:here/", "/|:") == "here/world:hello/"
+    assert reverse("hello//world:here", "//|:") == "here//world:hello"
